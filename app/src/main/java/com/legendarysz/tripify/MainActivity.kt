1886e3f51b347ec84f29fdd5986c1f43cb751fb7
Shahbaz Tariq <shahbaztariqb@gmail.com>
2024-02-24 19:17:39 +0500

    Started (1st Screen done)

---
+++ app/src/main/java/com/legendarysz/tripify/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.legendarysz.tripify
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn:ConstraintLayout = findViewById(R.id.clBtn)
+
+        btn.setOnClickListener{
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
